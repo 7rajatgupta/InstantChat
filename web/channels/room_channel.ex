8e3a851c546b0ccd56a5da4cfc9254001f1a3159
rajatpunstaa <rajatpunkstaa@gmail.com>
2017-08-04 02:20:48 +0530

    Initial

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -1,4 +1,12 @@
 #Created to handle the user channel.
 defmodule InstantChat.RoomChannel do
   use Phoenix.Channel
+
+  #Method to join the chat lobby:
+  def join("rooms: lobby", message, socket)do
+    {:ok, socket}
+  end
+  def join(_room, _param, _socket)do
+    {:error, %{reason: "Oops !! You can only enter the lobby !"}}
+  end
 end
